db5c5fdf19cb994facc7edf1deb2738f80fa5d12
Danial Al Ghazali Walangadi <danialagw@students.unnes.ac.id>
2024-05-20 16:57:15 +0700

    Tugas pendahuluan 1 gambar 12.5 Adjacency Matrix

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ struct Node {
 struct Graph {
     std::vector<Node> nodes; //Store each node's value for more elegant print()
     int** matrix; //THE matrix (Array of pointers (The column) that points to an array (The row))
+    //Non-dynamically allocated matrix cannot be declared without initialization, hence I used int** matrix
     int size; //THE matrix's size
 
     //Allocates memory for the 2D array
@@ -34,16 +35,16 @@ struct Graph {
     }
 
     //Add an edge (1 = there is an edge, 0 = no edge)
-    void addEdge(int sourceRow, int destinationColumn, int weight) {
-        if (sourceRow >= 0 && sourceRow < size && destinationColumn >= 0 && destinationColumn < size) {
-            matrix[sourceRow][destinationColumn] = weight;
+    void addEdge(int sourceColumn, int destinationRow, int weight) {
+        if (sourceColumn >= 0 && sourceColumn < size && destinationRow >= 0 && destinationRow < size) {
+            matrix[sourceColumn][destinationRow] = weight;
         }
     }
 
     //Check if an edge exists
-    bool checkEdge(int sourceRow, int destinationColumn) {
-        if (sourceRow >= 0 && sourceRow < size && destinationColumn >= 0 && destinationColumn < size) {
-            return matrix[sourceRow][destinationColumn] > 0;
+    bool checkEdge(int sourceColumn, int destinationRow) {
+        if (sourceColumn >= 0 && sourceColumn < size && destinationRow >= 0 && destinationRow < size) {
+            return matrix[sourceColumn][destinationRow] > 0;
         } else {
             return false;
         }
@@ -75,13 +76,16 @@ int main() {
     test.addNode(4); // Index of 3
     test.addNode(5); // Index of 4
 
-    test.addEdge(0, 1,3); //Edge that connects A and B
-    test.addEdge(1, 2,3); //Edge that connects B and C
-    test.addEdge(1, 4,3); //Bla bla bla
-    test.addEdge(2, 3,3);
-    test.addEdge(2, 4,3);
-    test.addEdge(4, 1,3);
-    test.addEdge(4, 2,3);
+    test.addEdge(0, 1,10); //Edge that connects A and B
+    test.addEdge(0, 3,5); //Edge that connects B and C
+    test.addEdge(1, 2,1); //...
+    test.addEdge(1, 3,2);
+    test.addEdge(2, 4,4);
+    test.addEdge(3, 1,3);
+    test.addEdge(3, 2,9);
+    test.addEdge(3, 4,2);
+    test.addEdge(4, 0,7);
+    test.addEdge(4, 2,6);
 
     test.print();
 }
